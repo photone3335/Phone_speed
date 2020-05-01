08254c54ac4189a029347d481c4e7b66f7fbd3b8
DragonOfVoid <47323076+DragonOfVoid@users.noreply.github.com>
2020-05-01 07:49:40 +0300

    Add files via upload

--- phone_speed/src/speed.cpp
+++ phone_speed/src/speed.cpp
@@ -1,5 +1,9 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Imu.h"
+#include "nav_msgs/Odometry.h"
+
+
+
 
 using namespace ros;
 
@@ -9,29 +13,19 @@ double vecOld[3]={0,0,0};
 double timeOld=0;
 double vel;
 
-void phoneCallback(const sensor_msgs::Imu::ConstPtr& data)
+Publisher imuPub;
+
+void poseCallback(const nav_msgs::Odometry::ConstPtr& data)
 {
-	if(F)
-	{	
-		timeOld=data->header.stamp.sec*1.0+data->header.stamp.nsec/1000000000.0;
-		vecOld[0]=data->orientation.x/data->orientation.w;
-		vecOld[1]=data->orientation.y/data->orientation.w;
-		vecOld[2]=data->orientation.z/data->orientation.w;
-		F=0;
-		return;
-	}	
-	double vec[3];
-	double time=data->header.stamp.sec+data->header.stamp.nsec/1000000000.0;
-	vec[0]=data->orientation.x/data->orientation.w;
-	vec[1]=data->orientation.y/data->orientation.w;
-	vec[2]=data->orientation.z/data->orientation.w;
-	
-	vel=(vec[0]-vecOld[0])*(vec[0]-vecOld[0])+(vec[1]-vecOld[1])*(vec[1]-vecOld[1])+(vec[2]-vecOld[2])*(vec[2]-vecOld[2])/(time-timeOld);
-	ROS_INFO("vel :%0.5f, time:%f",vel,time-timeOld);
-	vecOld[0]=vec[0];
-	vecOld[1]=vec[1];
-	vecOld[2]=vec[2];
-	timeOld=time;
+	ROS_INFO("%f", data->pose.pose.position.x);
+	return;
+}
+
+void imuRedirect(const sensor_msgs::Imu::ConstPtr& data)
+{
+	sensor_msgs::Imu corrected = *data;
+	corrected.header.frame_id = "base_footprint";
+	imuPub.publish(corrected);
 }
 
 int main(int argc, char **argv)
@@ -39,8 +33,13 @@ int main(int argc, char **argv)
     init(argc, argv, "speed");
 
 	NodeHandle node;
+	imuPub = node.advertise<sensor_msgs::Imu>("imu_data", 10);
 	
-    Subscriber imuSub = node.subscribe("android/imu", 10, &phoneCallback);
+	Subscriber imuSub = node.subscribe("/android/imu", 10, &imuRedirect);
+		
+    Subscriber poseSub = node.subscribe("/odometry/filtered", 10, &poseCallback);
+
+	
 
     Rate rate(10);
     ROS_INFO("Started");
@@ -50,3 +49,51 @@ int main(int argc, char **argv)
     	
     }
 }
+/*
+#include "ros/ros.h"
+#include "sensor_msgs/Imu.h"
+#include "_msgs/NavSatFix.h"
+
+
+
+
+using namespace ros;
+
+bool F=1;
+
+double vecOld[3]={0,0,0};
+double timeOld=0;
+double vel;
+
+Publisher imuPub, satPub, magPub;
+
+void imuCallback(const sensor_msgs::Imu::ConstPtr& data)
+{
+	imuPub.publish(data);
+}
+
+int main(int argc, char **argv)
+{
+    init(argc, argv, "speed");
+
+	NodeHandle node;
+	
+	imuPub = node.advertise<sensor_msgs::Imu>("/imu/data", 10);
+	//satPub = node.advertise<sensor_msgs::NavSatFix>("/sat/data", 10);
+	//magPub = node.advertise<sensor_msgs::MagneticField>("/imu/data", 10);
+	
+    Subscriber poseSub = node.subscribe("", 10, &imuCallback);
+	//Subscriber satSub = node.subscribe("android/fix", 10, &satCallback);
+	//Subscriber imuSub = node.subscribe("android/imu", 10, &phoneCallback);
+
+	
+
+    Rate rate(10);
+    ROS_INFO("Started");
+    while (ok()) {
+		spinOnce();
+        rate.sleep();
+    	
+    }
+}
+*/
